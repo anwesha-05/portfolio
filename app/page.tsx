f9a7a62a14bd3c7925a6b1edfb16e7b5a641d49d
Anwesha Satpathy <anwesha.satpathy2022@vitstudent.ac.in>
2025-02-03 23:19:20 +0530

    Fixed Next.js linting problems

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useRef, useEffect, useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import Link from "next/link";
 
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function Projects() {
     return (
       <div className="min-h-screen  bg-[#ff59bf] relative">
         {/* Navbar */}
         <nav className="w-full bg-[#ff59bf] p-4 pl-8 flex justify-start gap-8 text-lg font-mono fixed top-0 left-0 right-0" style={{ fontFamily: "'VT323', monospace", color: "white" }}>
-          <a href="/" className="hover:underline">Home</a>
-          <a href="/projects" className="hover:underline">Projects</a>
-          <a href="/about" className="hover:underline">About Me</a>
-          <a href="/resume" className="hover:underline">Resume</a>
-          <a href="/contact" className="hover:underline">Contact</a>
+        <Link href="/" className="hover:underline">Home</Link>
+          <Link href="/projects" className="hover:underline">Projects</Link>
+          <Link href="/about" className="hover:underline">About Me</Link>
+          <Link href="/resume" className="hover:underline">Resume</Link>
+          <Link href="/contact" className="hover:underline">Contact</Link>
         </nav>
         
         {/* Content */}
